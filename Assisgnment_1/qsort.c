353abc2a4c2bedda4b90795ad338d44d3d11cf7d
aviaryal <avinash.aryal@mavs.uta.edu>
2020-09-12 00:36:01 -0500

    first

---
+++ Assisgnment_1/qsort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int values[] = { 88, 56, 100, 2, 25 };
+
+int cmpfunc (const void * a, const void * b) {
+   return ( *(int*)a - *(int*)b );
+}
+
+int main () {
+   int n;
+
+   printf("Before sorting the list is: \n");
+   for( n = 0 ; n < 5; n++ ) {
+      printf("%d ", values[n]);
+   }
+   int x=0;
+   printf("Input array to sort");
+   scanf("%d",&x);
+   qsort(values, x, sizeof(int), cmpfunc);
+
+   printf("\nAfter sorting the list is: \n");
+   for( n = 0 ; n < 5; n++ ) {   
+      printf("%d ", values[n]);
+   }
+  
+   return(0);
+}
